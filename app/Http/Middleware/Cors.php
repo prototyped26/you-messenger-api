e827bb53bb5bd43561862732ffeded6786d90993
Allinone Advisor <allinoneadvisor@gmail.com>
2018-12-28 10:26:58 +0100

    Ajout de OPTIONS

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -20,6 +20,6 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
+            ->header('Access-Control-Allow-Headers', 'Content-Type, content-Type, Accept, Authorization, X-Requested-With, Application');
     }
 }
